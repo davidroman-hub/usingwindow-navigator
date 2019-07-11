c464d081d66b2487a49ad3bd636056ed394d6020
job david roman aguirre <jobroman83@gmail.com>
2019-07-11 08:41:25 -0500

    agregando la funcion en js de lineas

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -10,3 +10,28 @@ lienzo.moveTo(100, 100);
 lienzo.lineTo(200, 200);
 lienzo.stroke();
 lienzo.closePath(); // cierran un dibujo
+
+
+// segunda linea de diferente color
+
+lienzo.beginPath(); //arrancan un dibujo
+lienzo.strokeStyle = "blue";
+lienzo.moveTo(50, 10);
+lienzo.lineTo(290, 200);
+lienzo.stroke();
+lienzo.closePath(); // cierran un dibujo
+
+// ejemplo de funcion para no estar haciendo linea por linea 
+
+function dibujarlinea(color, xinicial, yinicial, xfinal, yfinal)
+{
+    lienzo.beginPath(); //arrancan un dibujo
+    lienzo.strokeStyle = color;
+    lienzo.moveTo(xinicial, yinicial);
+    lienzo.lineTo(xfinal, yfinal);
+    lienzo.stroke();
+    lienzo.closePath(); // cierran un dibujo
+}
+
+dibujarlinea ("pink", 10, 300, 220, 10, );
+dibujarlinea ("orange", 100, 200, 120, 200, );
